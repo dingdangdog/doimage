ddea8222221f43045b8566efac47bb28fb300e8f
dddx <dingdangdogx@outlook.com>
2025-02-18 14:25:32 +0800

    fix: update ImageCard component to improve loading behavior and styling

--- components/ImageCard.vue
+++ components/ImageCard.vue
@@ -2,13 +2,13 @@
 const { image } = defineProps<{ image?: string }>();
 const loading = ref(true);
 
-const onLoad = () => {
+onMounted(() => {
   loading.value = false;
-};
+});
 </script>
 
 <template>
-  <div class="m-2 w-[10rem] h-[8rem] border border-[#f6467c] rounded-sm">
+  <div class="m-2 w-40 h-40 border border-[#f6467c] rounded-sm">
     <div
       class="relative image bg-[rgba(242,197,211,0.5)] p-2 rounded-sm cursor-pointer overflow-hidden"
     >
@@ -16,12 +16,11 @@ const onLoad = () => {
         v-if="!loading"
         :src="image"
         alt="image card"
-        class="aspect-square w-full h-full object-cover rounded-sm"
-        @load="onLoad"
+        class="aspect-square w-full h-full object-contain rounded-sm"
       />
       <div
         v-if="loading"
-        class="absolute inset-0 flex justify-center items-center rounded-sm bg-gray-100"
+        class="absolute inset-0 w-full h-full flex justify-center items-center rounded-sm bg-gray-100"
       >
         <div
           class="w-12 h-12 h-12 border-b-2 border-gray-300 border-opacity-50 rounded-full animate-spin"
